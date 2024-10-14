b4b2e9afb1d6cb4e86e0917bc0a86e2344c62c0a
Damian Michałowski <58490484+damianmichalowski@users.noreply.github.com>
2024-10-15 01:58:51 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+import "./style.css";
+import { NewTodoForm } from "./NewTodoForm";
+import { TodoList } from "./TodoList";
+
+export default function App() {
+	const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue == null) return []
+
+    return JSON.parse(localValue)
+  });
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos])
+
+	function addTodo(title) {
+		setTodos((currenTodos) => {
+			return [
+				...currenTodos,
+				{ id: crypto.randomUUID(), title, completed: false },
+			];
+		});
+	}
+
+	function toggleTodo(id, completed) {
+		setTodos((currenTodos) => {
+			return currenTodos.map((todo) => {
+				if (todo.id === id) {
+					return { ...todo, completed };
+				}
+
+				return todo;
+			});
+		});
+	}
+
+	function deleteTodo(id) {
+		setTodos((currenTodos) => {
+			return currenTodos.filter((todo) => todo.id !== id);
+		});
+	}
+
+	console.log(todos);
+
+	return (
+		<>
+			<NewTodoForm onSubmit={addTodo} />
+			<h1 className="header"> TodoList</h1>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
+		</>
+	);
+}
